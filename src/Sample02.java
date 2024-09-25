584f72902471d7ddf7c50997a6d80d199894a4d1
ppgg-syrk <reoma.pugya@gmai.com>
2024-09-25 22:06:48 +0900

    Javaプログラミング入門学習終了

---
+++ src/Sample02.java
@@ -0,0 +1,18 @@
+
+public class Sample02 {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        int num1 = 100;
+        int num2 = 10;
+        int result;
+        result = sumMethod2(num1, num2);
+        System.out.println(result + "method2");
+    }
+    
+        public static int sumMethod2(int num1, int num2) {
+            int num= num1 + num2;
+            return num;
+    }
+
+}
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+
+    public static void main2(String[] args) {
+        System.out.println("TechAcademy");
+        System.out.println("Java");
+        System.out.println("学習を始めましょう!");
+    }
+}
